137d273e567beee69434d92c51d14f126e3cb7b3
Akashdeep <codechitra@gmail.com>
2021-12-06 11:07:29 +0530

    Added Some Comments For Better Understanding Of My Project

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ showNotes();
 //Adding Notes 
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function (e) {
+
+    // Targetting Txtarea to add notes
     let addTxt = document.getElementById("addTxt");
+
+    // Getting Notes From Local Storage In a Variable as a string
     let notes = localStorage.getItem("notes");
 
     if (notes == null) {
@@ -31,6 +35,8 @@ function showNotes() {
     }
 
     let html = ``;
+
+    // noteObj is an array that is made to store notes
     noteObj.forEach(function (element, index) {
         html += `
         <div class="card noteCard" style="width: 18rem;">
@@ -42,6 +48,8 @@ function showNotes() {
       </div>`;
 
     });
+
+    // notesBox is the container in which we will show our notes
     let notesBox = document.getElementById("notesBox");
     if (noteObj.length != 0) {
         notesBox.innerHTML = html;
@@ -72,11 +80,18 @@ function deleteNote(index) {
 
 //Search Button Functionality
 
+// Here we are targetting search box
 let searchTxt = document.getElementById("searchTxt");
+
+// Now we will be using input Event
 searchTxt.addEventListener("input", function () {
     let inputVal = searchTxt.value.toLowerCase();
+
+    // Here We Are Targetting Our Cards Which Are Notes By Class Name notecard|| Means It will return an html collection of all cards
     let elem = document.getElementsByClassName("noteCard");
     Array.from(elem).forEach(function (element) {
+
+        // Here element means each card as we are iterating our array and we are targetting each card's paragraph tag
         let cardTxt = element.getElementsByTagName("p")[0].innerText.toLowerCase();
         // console.log(cardTxt);
         if (cardTxt.includes(inputVal)) {
